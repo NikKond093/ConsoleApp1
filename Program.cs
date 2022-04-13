bdefb414e937ee180a7f4b03ce7d9de9461748ca
ADMIN <ADMIN@DESKTOP-OEIQSEI>
2022-04-13 21:20:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte moneyRuble = 1;
+        sbyte moneyDollar = -1;
+        short weightBox = 32767;
+        ushort weightWolverine = 20;
+        int lengthTriangle = 100;
+        uint lengthOval = 4294967295;
+        long priceMercedesBenz = 5000000;
+        string myName = "Nikita";
+        double sizeShirt = 50.00;
+        float sizeJeans = 40.00F;
+    }
+}
